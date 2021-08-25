161a336b9c5ef7130e6e7e030236c389f7865fd8
truong.duc.manh <ducmanh2111@gmail.com>
2021-08-25 21:45:11 +0700

    change_max_number_thread

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -11,7 +11,7 @@ public class Server {
     private static String[] adjs = {"the gentle", "the un-gentle", "the overwrought"};
 
     private static ClientHandler[] clients = new ClientHandler[100];
-    private static ExecutorService pool = Executors.newFixedThreadPool(4);
+    private static ExecutorService pool = Executors.newFixedThreadPool(2);
     public static int clientId = 0;
 
     public static void main(String[] args) throws IOException {
